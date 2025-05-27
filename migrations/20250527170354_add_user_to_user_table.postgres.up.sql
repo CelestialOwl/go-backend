a4e0b477ae3b34a1fa516a569212d0f8be2e63ad
hassan <cm.hassan98@gmail.com>
2025-05-27 23:01:08 +0500

    add the migration for the user data

---
+++ migrations/20250527170354_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (email,first_name,last_name,"password",access_level,created_at,updated_at) VALUES
+	 ('hassan@test.com','Hassan','Akhtar','$2a$12$ERf29yGaCZ3n4kmvoTAKPuGojJk2O2ZN.qBb1Kxvozm/rZU2x9b7a',3,'2025-04-30 00:00:00','2025-08-30 00:00:00');
